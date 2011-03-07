dc08f652f60723952f772c455c7441c7c095ca21
cxxjoe <cxxjoe@eb22bf75-c9a7-79b4-85cc-f1f1cf25690d>
2011-03-07 15:45:30 +0000

    Allow cmake -DCMAKE_BUILD_TYPE=Debug builds with debug symbols and stuff.

--- src/common/BuzzLogger.h
+++ src/common/BuzzLogger.h
@@ -7,6 +7,7 @@
 #ifndef _BUZZ_LOGGER_H
 #define _BUZZ_LOGGER_H
 
+#include "buzzconfig.h"
 #include "Logger.h"
 #include <boost/shared_ptr.hpp>
 #include <sstream>
@@ -14,8 +15,18 @@
 // needs to be instanstiated in main():
 extern boost::shared_ptr<CLogger> _s_BuzzLogger;
 
-// use this from everywhere!
-#define BUZZ_LOG(LEVEL, ARGS) do { \
+// use the BUZZ_LOG and BUZZ_LOG_THIS macros from everywhere!
+
+// ugly preprocessor magic below this line.
+
+#ifdef _DEBUG
+	#define BUZZ_LOG(LEVEL, ARGS) _BUZZLOG(LEVEL, ARGS)
+#else
+	// allow compiler to optimize away DEBUG log messages:
+	#define BUZZ_LOG(LEVEL, ARGS) if(LEVEL != CLogger::LL_DEBUG) { _BUZZLOG(LEVEL, ARGS); }
+#endif
+
+#define _BUZZ_LOG(LEVEL, ARGS) do { \
 		std::stringstream _s_log; \
 		_s_log << ARGS; \
 		::_s_BuzzLogger->Log(LEVEL, _s_log.str()); \
